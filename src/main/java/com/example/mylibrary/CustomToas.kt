8eae19505dac0f0c248f206d1ba5bbffd9822ebf
NgocTranQuang <ngoctq110309@gmail.com>
2022-05-24 09:18:28 +0700

    Add files via upload
    i

---
+++ src/main/java/com/example/mylibrary/CustomToas.kt
@@ -0,0 +1,11 @@
+package com.example.mylibrary
+
+import android.util.Log
+
+class CustomToas {
+    companion object{
+        fun showToast(){
+            Log.d("CustomToas", "showToast: ")
+        }
+    }
+}
